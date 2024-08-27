47263b756b3d59bed75f20335f78b8dec20eae20
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-28 02:27:06 +0530

    1.3

--- src/components/App.js
+++ src/components/App.js
@@ -86,19 +86,19 @@ const App = () => {
         <h1 id="title">Our Menu</h1>
         <div id="underline"></div>
         <ul id="nav">
-          <li  className='nav-item' data-test-id="menu-item-all"
+          <li id="filter-btn-0"  data-test-id="menu-item-all"
             onClick={() => setMenuItems(data)}
           >All</li>
 
-          <li  className='nav-item' data-test-id="menu-item-breakfast"
+          <li id="filter-btn-1"  data-test-id="menu-item-breakfast"
             onClick={() => setMenuItems(data.filter(item => item.category === 'breakfast'))}
           >Breakfast</li>
 
-          <li  className='nav-item' data-test-id="menu-item-lunch"
+          <li id="filter-btn-2"  data-test-id="menu-item-lunch"
             onClick={() => setMenuItems(data.filter(item => item.category === 'lunch'))}
           >Lunch</li>
 
-          <li  className='nav-item' data-test-id="menu-item-shakes"
+          <li id="filter-btn-3"  data-test-id="menu-item-shakes"
             onClick={() => setMenuItems(data.filter(item => item.category === 'shakes'))}
           >Shakes</li>
         </ul>
